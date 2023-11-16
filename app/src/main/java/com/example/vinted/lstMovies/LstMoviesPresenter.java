592737b714501299f472228d8f76288561b9c471
Carlos Fernández Guevara <131338727+CarlosFerGue@users.noreply.github.com>
2023-11-16 19:23:06 +0100

    ps

--- app/src/main/java/com/example/vinted/lstMovies/LstMoviesPresenter.java
+++ app/src/main/java/com/example/vinted/lstMovies/LstMoviesPresenter.java
@@ -1,22 +1,29 @@
 package com.example.vinted.lstMovies;
 
+import com.example.vinted.lstMovies.model.LstMoviesModel;
 import com.example.vinted.lst_movies_2.beans.Pelicula;
+import com.example.vinted.lst_movies_2.view.LstPelicula2Activity;
 
 import java.util.ArrayList;
 
 public class LstMoviesPresenter  implements ContractListMovies.Presenter,
                                 ContractListMovies.Model.OnLstMoviesListener{
+
     private ContractListMovies.View vista;
     private LstMoviesModel lstMoviesModel;
 
+    public LstMoviesPresenter(ContractListMovies.View vista){
+        this.vista = vista;
+        lstMoviesModel = new LstMoviesModel(this);
+    }
     @Override
     public void lstMovies(String filtro) {
-
+        lstMoviesModel.moviesAPI("", this);
     }
 
     @Override
     public void onFinished(ArrayList<Pelicula> lstPelicula) {
-
+        vista.successMovies(lstPelicula);
     }
 
     @Override
--- app/src/main/java/com/example/vinted/lst_movies_2/view/LstPelicula2Activity.java
+++ app/src/main/java/com/example/vinted/lst_movies_2/view/LstPelicula2Activity.java
@@ -1,24 +1,48 @@
 package com.example.vinted.lst_movies_2.view;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.vinted.R;
+import com.example.vinted.lstMovies.LstMoviesPresenter;
+import com.example.vinted.lstMovies.model.LstMoviesModel;
 import com.example.vinted.lst_movies_2.ContractListMovies;
 import com.example.vinted.lst_movies_2.beans.Pelicula;
+
 
 import java.util.ArrayList;
 
 public class LstPelicula2Activity extends AppCompatActivity
         implements ContractListMovies.View {
-    //Activity es la pantalla que muestras
 
+    //Activity es la pantalla que muestras
     private LstMoviesPresenter lstMoviesPresenter;
 
-    public void onCreate(Bundle savedInstanceState){
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lst_pelicula2);
 
+        LstMoviesPresenter lstMoviesPresenter = new LstMoviesPresenter(this);
+        lstMoviesPresenter.lstMovies("");
+
+        Button btnEnviar = findViewById(R.id.btnEnviar);
+        btnEnviar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                lstMoviesPresenter.lstMovies("");
+            }
+        });
     }
 
     @Override
     public void successMovies(ArrayList<Pelicula> lstPelicula) {
+        Toast.makeText(this, lstPelicula.get(0), Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, lstPelicula.get(0).getDescripcion(), Toast.LENGTH_SHORT).show();
 
     }
 
