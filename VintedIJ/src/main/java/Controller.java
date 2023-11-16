0b0bb73df702a99b8ba636a78aee00519161edf8
Carlos Fernández Guevara <131338727+CarlosFerGue@users.noreply.github.com>
2023-11-16 19:37:23 +0100

    sa

--- VintedIJ/src/main/java/Controller.java
+++ VintedIJ/src/main/java/Controller.java
@@ -1,3 +1,24 @@
-public class Controller {
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class Controller {
+    public static void ejecutar(String tabla, String action) throws SQLException, ParserConfigurationException, IOException, SAXException {
+        switch (tabla){
+            case "ARTICULOS":
+                new ArticulosDAO().ejecutar(action);
+                break;
+            case "PARTICULARES":
+                new ParticularesDAO().ejecutar(action);
+                break;
+            case "TICKETS":
+                new TicketsDAO().ejecutar(action);
+                break;
+            case "DIRECCIONES":
+                new DireccionesDAO().ejecutar(action);
+                break;
+        }
+    }
 }
