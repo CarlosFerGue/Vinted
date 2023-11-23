45433052fe7854b09670de5b55b28096c192f70d
Carlos Fernández Guevara <131338727+CarlosFerGue@users.noreply.github.com>
2023-11-23 21:29:48 +0100

    s

--- Android/app/src/main/java/com/example/myapplication/loggedScreen/adapter/OnUser10Adapter.java
+++ Android/app/src/main/java/com/example/myapplication/loggedScreen/adapter/OnUser10Adapter.java
@@ -1,20 +1,37 @@
 package com.example.myapplication.loggedScreen.adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.myapplication.R;
+
+import java.util.ArrayList;
+
 public class OnUser10Adapter extends RecyclerView.Adapter<OnUser10Adapter.ViewHolder> {
+    private ArrayList<OnUser10Adapter> lstUsers;
+    private LayoutInflater inflater;
+
+    public OnUser10Adapter(Context context, ArrayList<OnUser10Adapter> lstUsers){
+        this.lstUsers = lstUsers;
+        this.inflater = LayoutInflater.from(context);
+    }
+
+
     @NonNull
     @Override
     public OnUser10Adapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = inflater.inflate(R.layout.user_carta, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull OnUser10Adapter.ViewHolder holder, int position) {
-
+        
     }
 
     @Override
@@ -22,6 +39,7 @@ public class OnUser10Adapter extends RecyclerView.Adapter<OnUser10Adapter.ViewHo
         return 0;
     }
 
-    public class ViewHolder {
+    public class ViewHolder extends  RecyclerView.ViewHolder{
+
     }
 }
