e761e8f0ff30d514347f02e51c4802eef58d80cb
Carlos Fernández Guevara <131338727+CarlosFerGue@users.noreply.github.com>
2023-12-12 08:41:19 +0100

    ses

--- Android/app/src/main/java/com/example/myapplication/usuario/comprasUsuario/historicoCompras/adapter/OnHistoricoAdapter.java
+++ Android/app/src/main/java/com/example/myapplication/usuario/comprasUsuario/historicoCompras/adapter/OnHistoricoAdapter.java
@@ -1,4 +1,24 @@
 package com.example.myapplication.usuario.comprasUsuario.historicoCompras.adapter;
 
-public class OnHistoricoAdapter {
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class OnHistoricoAdapter extends RecyclerView.Adapter<OnHistoricoAdapter.ViewHolder> {
+    @NonNull
+    @Override
+    public OnHistoricoAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull OnHistoricoAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
