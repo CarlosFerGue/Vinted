8f40712e6c146ca66f4a69b605967d01d61d4424
Carlos Fernández Guevara <131338727+CarlosFerGue@users.noreply.github.com>
2023-12-12 08:52:43 +0100

    sa

--- Android/app/src/main/java/com/example/myapplication/usuario/comprasUsuario/historicoCompras/adapter/OnHistorialAdapter.java
+++ Android/app/src/main/java/com/example/myapplication/usuario/comprasUsuario/historicoCompras/adapter/OnHistorialAdapter.java
@@ -2,7 +2,11 @@ package com.example.myapplication.usuario.comprasUsuario.historicoCompras.adapte
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -27,17 +31,53 @@ public class OnHistorialAdapter extends RecyclerView.Adapter<OnHistorialAdapter.
     @NonNull
     @Override
     public OnHistorialAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = inflater.inflate(R.layout.sale_data_carta_vendida)
-        return null;
+        View view = inflater.inflate(R.layout.sale_data_carta_vendida, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull OnHistorialAdapter.ViewHolder holder, int position) {
+        OnHistorialData prod = lstProd.get(position);
+
+        //Rellenamos los datos de la carta
+        holder.productName.setText(lstProd.get(position).getNombre());
+        holder.productBrand.setText(lstProd.get(position).getMarca());
+        holder.productPrice.setText(lstProd.get(position).getPrecio());
+        holder.productState.setText(lstProd.get(position).getEstado());
+        holder.productDesc.setText(lstProd.get(position).getDescripcion());
+        holder.productRate.setText(lstProd.get(position).getValoracion());
+        holder.addRateEditText.getText();
+
+
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return lstProd.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView productName;
+        TextView productBrand;
+        TextView productPrice;
+        TextView productDesc;
+        TextView productState;
+        TextView productColor;
+        TextView productRate;
+        EditText addRateEditText;
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            productName = itemView.findViewById(R.id.productDesc);
+            productBrand = itemView.findViewById(R.id.productBrand);
+            productPrice = itemView.findViewById(R.id.productState);
+            productDesc = itemView.findViewById(R.id.textProductView);
+            productState = itemView.findViewById(R.id.productPrice);
+            productColor = itemView.findViewById(R.id.productColor);
+            productRate = itemView.findViewById(R.id.productRate);
+
+        }
     }
 }
