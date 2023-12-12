294eae8672f89cb9fcc5d6c1829b000737bc1f01
Carlos Fernández Guevara <131338727+CarlosFerGue@users.noreply.github.com>
2023-12-12 08:10:44 +0100

    dsas

---
+++ Android/app/src/main/java/com/example/myapplication/usuario/comprasUsuario/historicoCompras/adapter/OnAllProdAdapter.java
@@ -0,0 +1,127 @@
+package com.example.myapplication.usuario.comprasUsuario.historicoCompras.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.myapplication.R;
+import com.example.myapplication.articulo.allProducts.data.OnAllProdData;
+import com.example.myapplication.beans.Producto;
+import com.example.myapplication.usuario.comprasUsuario.comprar.presenter.ComprarPresenter;
+import com.example.myapplication.usuario.updateRating.presenter.UpdateRatePresenter;
+
+import java.util.ArrayList;
+
+public class OnAllProdAdapter extends RecyclerView.Adapter<OnAllProdAdapter.ViewHolder> {
+    private int idUsuario;
+    private ArrayList<OnAllProdData> lstProd;
+    private LayoutInflater inflater;
+
+
+
+    public OnAllProdAdapter(Context context, ArrayList<OnAllProdData> lstProd, int idUsuario) {
+        this.lstProd = lstProd;
+        this.inflater = LayoutInflater.from(context);
+        this.idUsuario = idUsuario;
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.sale_data_carta, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        OnAllProdData prod = lstProd.get(position);
+
+        //Rellenamos los datos de la carta
+        holder.productName.setText(lstProd.get(position).getNombre());
+        holder.productBrand.setText(lstProd.get(position).getMarca());
+        holder.productPrice.setText(lstProd.get(position).getPrecio());
+        holder.productState.setText(lstProd.get(position).getEstado());
+        holder.productDesc.setText(lstProd.get(position).getDescripcion());
+        holder.productRate.setText(lstProd.get(position).getValoracion());
+        holder.addRateEditText.getText();
+
+
+
+        //Cambiar puntuacion
+        holder.productButton.setOnClickListener(e -> {
+            System.out.println("Yeeeeesaaa All prod");
+            String addRateStr = holder.addRateEditText.getText().toString().trim();
+           Producto producto = new Producto(lstProd.get(position).getId_producto(),
+                    addRateStr);
+
+
+
+            System.out.println(addRateStr);
+            System.out.println(producto);
+
+            UpdateRatePresenter presenter = new UpdateRatePresenter();
+            presenter.updateRating(producto);
+
+        });
+
+
+        //Comprar
+        holder.botonComprar.setOnClickListener(e ->{
+            Producto producto = new Producto(lstProd.get(position).getId_producto(),
+                    idUsuario);
+
+            System.out.println("El usuarios es " + idUsuario);
+            System.out.println("El id del producto es " + lstProd.get(position).getId_producto());
+
+            ComprarPresenter presenter = new ComprarPresenter();
+            presenter.Compra(producto);
+        });
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return lstProd.size();
+    }
+
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView productName;
+        TextView productBrand;
+        TextView productPrice;
+        TextView productDesc;
+        TextView productState;
+        TextView productColor;
+        TextView productRate;
+        Button productButton;
+        Button botonComprar;
+        EditText addRateEditText;
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            productName = itemView.findViewById(R.id.productDesc);
+            productBrand = itemView.findViewById(R.id.productBrand);
+            productPrice = itemView.findViewById(R.id.productState);
+            productDesc = itemView.findViewById(R.id.textProductView);
+            productState = itemView.findViewById(R.id.productPrice);
+            productColor = itemView.findViewById(R.id.productColor);
+            productRate = itemView.findViewById(R.id.productRate);
+            productButton = itemView.findViewById(R.id.botonValoracion);
+            botonComprar = itemView.findViewById(R.id.botonComprar);
+            addRateEditText = itemView.findViewById(R.id.productRate);
+
+        }
+    }
+}
---
+++ Android/app/src/main/java/com/example/myapplication/usuario/comprasUsuario/historicoCompras/presenter/OnAllProdPresenter.java
@@ -0,0 +1,33 @@
+package com.example.myapplication.usuario.comprasUsuario.historicoCompras.presenter;
+
+import com.example.myapplication.articulo.allProducts.ContractAllProducts;
+import com.example.myapplication.articulo.allProducts.data.OnAllProdData;
+import com.example.myapplication.usuario.comprasUsuario.historicoCompras.model.OnAllProdModel;
+
+import java.util.ArrayList;
+
+public class OnAllProdPresenter implements ContractAllProducts.Presenter, ContractAllProducts.Model.loadAllProdListener{
+    ContractAllProducts.View view;
+    ContractAllProducts.Model model;
+
+    public OnAllProdPresenter(ContractAllProducts.View view){
+        this.view = view;
+        model = new OnAllProdModel(this);
+    }
+
+    @Override
+    public void LoadOnAllProd(int userId) {
+        model.loadAllProdAPI(userId, this);
+    }
+
+    @Override
+    public void onFinished(ArrayList<OnAllProdData> lstProd) {
+        view.succesLoadAllProd(lstProd);
+    }
+
+    @Override
+    public void onFailure(String err) {
+        view.failureLoadAllProd(err);
+    }
+
+}
